7acd3a3b9448450fd746dad994b5e8a33244bf40
Niklas Högefjord <niklas@krokedil.se>
2018-04-09 17:14:23 +0200

    Text domain fix for Select another payment method label

--- includes/class-wc-paysoncheckout-functions.php
+++ includes/class-wc-paysoncheckout-functions.php
@@ -56,7 +56,7 @@ function wc_payson_show_another_gateway_button() {
 
 	if ( count( $available_gateways ) > 1 ) {
 		$settings                   = get_option( 'woocommerce_payson_settings' );
-		$select_another_method_text = isset( $settings['select_another_method_text'] ) && '' !== $settings['select_another_method_text'] ? $settings['select_another_method_text'] : __( 'Select another payment method', 'klarna-checkout-for-woocommerce' );
+		$select_another_method_text = isset( $settings['select_another_method_text'] ) && '' !== $settings['select_another_method_text'] ? $settings['select_another_method_text'] : __( 'Select another payment method', 'woocommerce-gateway-paysoncheckout' );
 
 		?>
 		<p style="margin-top:30px">
