700f006740a4e59f485d65fbe97bca410bded9f7
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2019-12-06 17:25:20 +0100

    Better control for checking result from get_order request

--- classes/class-paysoncheckout-for-woocommerce-callbacks.php
+++ classes/class-paysoncheckout-for-woocommerce-callbacks.php
@@ -119,12 +119,20 @@ class PaysonCheckout_For_WooCommerce_Callbacks {
 			$payment_id   = $_GET['subscription'];
 			$subscription = true;
 		}
-		$payson_order = pco_wc_get_order( $payment_id, $subscription );
-		if ( 'readyToShip' === $payson_order['status'] || 'customerSubscribed' === $payson_order['status'] ) {
-			PaysonCheckout_For_WooCommerce_Logger::log( 'Notification Listener hit: ' . json_encode( $_GET ) . ' URL: ' . $_SERVER['REQUEST_URI'] );
-			wp_schedule_single_event( time() + 120, 'pco_check_for_order', array( $payment_id, $subscription ) );
+
+		if ( isset( $payment_id ) ) {
+
+			$payson_order = pco_wc_get_order( $payment_id, $subscription );
+			if ( is_wp_error( $payson_order ) ) {
+				PaysonCheckout_For_WooCommerce_Logger::log( 'Could not get order in notification callback. Payment ID: ' . $payment_id . 'Is subscription order: ' . $subscription );
+			} else {
+				if ( 'readyToShip' === $payson_order['status'] || 'customerSubscribed' === $payson_order['status'] ) {
+					PaysonCheckout_For_WooCommerce_Logger::log( 'Notification Listener hit: ' . json_encode( $_GET ) . ' URL: ' . $_SERVER['REQUEST_URI'] );
+					wp_schedule_single_event( time() + 120, 'pco_check_for_order', array( $payment_id, $subscription ) );
+				}
+				header( 'HTTP/1.1 200 OK' );
+			}
 		}
-		header( 'HTTP/1.1 200 OK' );
 	}
 
 	public function pco_check_for_order_callback( $payment_id, $subscription ) {
