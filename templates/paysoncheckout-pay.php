bfb0b955ad4ba588d5162c3f082d3e011041df30
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-06-10 16:18:23 +0200

    Add show pay for order snippet function

---
+++ templates/paysoncheckout-pay.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Billmate_Checkout pay for order page.
+ *
+ * Overrides /checkout/form-pay.php.
+ *
+ * @package Billmate_Checkout/Templates
+ */
+
+?>
+<div id="pco-iframe">
+	<?php pco_wc_show_pay_for_order_snippet(); ?>
+</div>
