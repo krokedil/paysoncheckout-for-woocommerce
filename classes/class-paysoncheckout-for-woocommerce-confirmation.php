7bf55a48d6e2c7155bf8df07e01def39aeea34f4
Miodrag Stojanovic <mio.sto@yandex.com>
2020-07-07 14:49:31 +0200

    add init action for confirming payson order

--- classes/class-paysoncheckout-for-woocommerce-confirmation.php
+++ classes/class-paysoncheckout-for-woocommerce-confirmation.php
@@ -35,7 +35,32 @@ class PaysonCheckout_For_WooCommerce_Confirmation {
 	 * Class constructor.
 	 */
 	public function __construct() {
-		
+		add_action( 'init', array( $this, 'pco_confirm_order' ) );
+	}
+
+	/**
+	 * Confirm order
+	 */
+	public function pco_confirm_order() {
+		$pco_confirm  = filter_input( INPUT_GET, 'pco_confirm', FILTER_SANITIZE_STRING );
+		$pco_order_id = filter_input( INPUT_GET, 'pco_order_id', FILTER_SANITIZE_STRING );
+		$order_key    = filter_input( INPUT_GET, 'key', FILTER_SANITIZE_STRING );
+
+		// Return if we dont have our parameters set.
+		if ( empty( $pco_confirm ) || empty( $pco_order_id ) || empty( $order_key ) ) {
+			return;
+		}
+
+		$order_id = wc_get_order_id_by_order_key( $order_key );
+
+		// Return if we cant find an order id.
+		if ( empty( $order_id ) ) {
+			return;
+		}
+
+		// Confirm the order.
+		pco_confirm_payson_order( $pco_order_id, $order_id );
+		pco_wc_unset_sessions();
 	}
 
 }
