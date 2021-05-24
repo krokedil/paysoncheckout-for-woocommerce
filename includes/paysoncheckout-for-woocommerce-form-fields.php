917b8873b12923e9d9e6d1b67d6c268cbd7ed7d8
Michael Bengtsson <michael@krokedil.se>
2021-05-24 10:43:01 +0200

    Add support for expiration date for recurring payments

--- includes/paysoncheckout-for-woocommerce-form-fields.php
+++ includes/paysoncheckout-for-woocommerce-form-fields.php
@@ -6,58 +6,65 @@
  */
 
 $settings = array(
-	'enabled'                    => array(
+	'enabled'                      => array(
 		'title'   => __( 'Enable/Disable', 'woocommerce-gateway-paysoncheckout' ),
 		'type'    => 'checkbox',
 		'label'   => __( 'Enable ' . $this->method_title, 'woocommerce-gateway-paysoncheckout' ), // phpcs:ignore
 		'default' => 'yes',
 	),
-	'title'                      => array(
+	'title'                        => array(
 		'title'       => __( 'Title', 'woocommerce-gateway-paysoncheckout' ),
 		'type'        => 'text',
 		'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce-gateway-paysoncheckout' ),
 		'default'     => __( $this->method_title, 'woocommerce-gateway-paysoncheckout' ), // phpcs:ignore
 		'desc_tip'    => true,
 	),
-	'description'                => array(
+	'description'                  => array(
 		'title'       => __( 'Description', 'woocommerce-gateway-paysoncheckout' ),
 		'type'        => 'textarea',
 		'default'     => __( 'Pay with Payson via invoice, card, direct bank payments, part payment and sms.', 'woocommerce-gateway-paysoncheckout' ),
 		'desc_tip'    => true,
 		'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce-gateway-paysoncheckout' ),
 	),
-	'select_another_method_text' => array(
+	'select_another_method_text'   => array(
 		'title'       => __( 'Other payment method button text', 'woocommerce-gateway-paysoncheckout' ),
 		'type'        => 'text',
 		'description' => __( 'Customize the <em>Select another payment method</em> button text that is displayed in checkout if using other payment methods than PaysonCheckout. Leave blank to use the default (and translatable) text.', 'woocommerce-gateway-paysoncheckout' ),
 		'default'     => '',
 		'desc_tip'    => true,
 	),
-	'merchant_id'                => array(
+	'recurring_invoice_expiration' => array(
+		'title'       => __( 'Recurring invoice expiration time', 'woocommerce-gateway-paysoncheckout' ),
+		'type'        => 'number',
+		'description' => __( 'Set the default expiration time for a recurring order invoice in days. This is only applicable if you are using WooCommerce Subscriptions.', 'woocommerce-gateway-paysoncheckout' ),
+		'default'     => '7',
+		'desc_tip'    => true,
+	),
+	'merchant_id'                  => array(
 		'title'       => __( 'Agent ID', 'woocommerce-gateway-paysoncheckout' ),
 		'type'        => 'text',
 		'description' => __( '', 'woocommerce-gateway-paysoncheckout' ), // phpcs:ignore
 		'default'     => '',
 	),
-	'api_key'                    => array(
+	'api_key'                      => array(
 		'title'       => __( 'API Key', 'woocommerce-gateway-paysoncheckout' ),
 		'type'        => 'text',
 		'description' => __( '', 'woocommerce-gateway-paysoncheckout' ), // phpcs:ignore
 		'default'     => '',
 	),
-	'testmode'                   => array(
+	'testmode'                     => array(
 		'title'   => __( 'Testmode', 'woocommerce-gateway-paysoncheckout' ),
 		'type'    => 'checkbox',
 		'label'   => __( 'Enable PaysonCheckout testmode', 'woocommerce-gateway-paysoncheckout' ),
 		'default' => 'no',
 	),
-	'order_management'           => array(
+	'order_management'             => array(
 		'title'   => __( 'Enable Order Management', 'woocommerce-gateway-paysoncheckout' ),
 		'type'    => 'checkbox',
 		'label'   => __( 'Enable Payson order capture on WooCommerce order completion and Payson order cancellation on WooCommerce order cancellation', 'woocommerce-gateway-paysoncheckout' ),
 		'default' => 'yes',
 	),
-	'color_scheme'               => array(
+	'color_scheme'                 => array(
 		'title'       => __( 'Color Scheme', 'woocommerce-gateway-paysoncheckout' ),
 		'type'        => 'select',
 		'options'     => array(
@@ -68,7 +75,7 @@ $settings = array(
 		'default'     => 'White',
 		'desc_tip'    => true,
 	),
-	'debug'                      => array(
+	'debug'                        => array(
 		'title'       => __( 'Debug Log', 'woocommerce-gateway-paysoncheckout' ),
 		'type'        => 'checkbox',
 		'label'       => __( 'Enable logging', 'woocommerce-gateway-paysoncheckout' ),
