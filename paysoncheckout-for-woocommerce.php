f8252bfbbd031a470eca1849fc241db6f1aac109
MichaelBengtsson <michael@krokedil.se>
2021-08-20 08:49:06 +0200

    Make __wakeup public for php 8.0

--- paysoncheckout-for-woocommerce.php
+++ paysoncheckout-for-woocommerce.php
@@ -80,7 +80,7 @@ if ( ! class_exists( 'PaysonCheckout_For_WooCommerce' ) ) {
 		 *
 		 * @return void
 		 */
-		private function __wakeup() {
+		public function __wakeup() {
 			wc_doing_it_wrong( __FUNCTION__, __( 'Nope' ), '1.0' );
 		}
 
