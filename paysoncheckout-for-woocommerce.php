ec38558e30e18677b6ffa27ed9c3fee39534f124
LindaRein <89569030+LindaRein@users.noreply.github.com>
2021-12-01 12:16:58 +0100

    Update paysoncheckout-for-woocommerce.php
    Bump tested WP/WC version WP version 5.8.2 WC version 6.0.0

--- paysoncheckout-for-woocommerce.php
+++ paysoncheckout-for-woocommerce.php
@@ -12,7 +12,7 @@
  * Domain Path:     /languages
  *
  * WC requires at least: 3.0
- * WC tested up to: 5.7.0
+ * WC tested up to: 6.0.0
  *
  * Copyright:       © 2016-2021 Krokedil.
  * License:         GNU General Public License v3.0
