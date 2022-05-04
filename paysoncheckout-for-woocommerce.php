ba011dd536529a59ffa2ff4db67759b3e5ed6628
Martin Lindahl <martin@krokedil.se>
2022-05-04 15:46:23 +0200

    WC WP bump after test

--- paysoncheckout-for-woocommerce.php
+++ paysoncheckout-for-woocommerce.php
@@ -12,7 +12,7 @@
  * Domain Path:     /languages
  *
  * WC requires at least: 3.0
- * WC tested up to: 6.4.0
+ * WC tested up to: 6.5.0
  *
  * Copyright:       © 2016-2022 Krokedil.
  * License:         GNU General Public License v3.0
