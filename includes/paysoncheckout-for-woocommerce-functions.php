11f1068ac9a8a641419c658c1055ca96251c6d39
Montazar Al-Jaber <montazar@duck.com>
2022-05-19 13:20:41 +0200

    Redirect to thankyou page if order is already completed

--- includes/paysoncheckout-for-woocommerce-functions.php
+++ includes/paysoncheckout-for-woocommerce-functions.php
@@ -106,6 +106,37 @@ function pco_wc_maybe_create_payson_order( $subscription = false ) {
 		if ( is_wp_error( $payson_order ) ) {
 			pco_wc_force_new_checkout_session();
 		}
+
+		/* The order might have already been completed, but the customer was not redirected to the confirmation page. */
+		if ( 'readyToShip' === $payson_order['status'] ) {
+			$order = wc_get_orders(
+				array(
+					'payment_method' => 'paysoncheckout',
+					'meta_key'       => '_payson_checkout_id',
+					'meta_value'     => WC()->session->get( 'payson_payment_id' ),
+					'meta_compare'   => '=',
+					'limit'          => 1,
+				),
+			);
+
+			if ( count( $order ) > 0 ) {
+
+				wp_safe_redirect(
+					add_query_arg(
+						array(
+							'key'          => $order[0]->get_order_key(),
+							'pco_confirm'  => 'yes',
+							'pco_order_id' => WC()->session->get( 'payson_payment_id' ),
+						),
+						$order[0]->get_checkout_order_received_url()
+					)
+				);
+
+				pco_confirm_payson_order( WC()->session->get( 'payson_payment_id' ), $order[0]->get_id() );
+				exit;
+			}
+		}
+
 		// Check if the order has a valid status and not on confirmation page or thank you page..
 		if ( ! is_order_received_page() && ! pco_check_valid_order_status( $payson_order ) && ! isset( $_GET['pco_confirm'] ) ) {
 			// If not clear session and rerun function to get a new order.
