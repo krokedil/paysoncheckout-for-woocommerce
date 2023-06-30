78648c143eb1dcbfc2edb8c97a649b13e0edb50d
MichaelBengtsson <michael@krokedil.se>
2023-06-30 16:35:23 +0200

    Minor code cleanup

--- tests/e2e/pages/PaysonIFrame.ts
+++ tests/e2e/pages/PaysonIFrame.ts
@@ -10,54 +10,31 @@ export class PaysonIFrame {
     }
 
     async fillPaysonPaymentDetails() {
+        // Fill in the payson iframe with the required information.
         await this.iframe.getByRole('textbox', { name: 'E-mail' }).fill('test.testsson@test.se');
         await this.iframe.getByLabel('Personal ID number').fill('4606082222');
         await this.iframe.getByRole('textbox', { name: 'Zip code' }).fill('99999');
         await this.iframe.locator('#PersonLookupMandatoryPhoneNumber').fill('0720000000');
     }
 
-    // async safeCheckout()
-    // {
-    //     while ( !this.page.url().includes('mock') ) 
-    //     {
-    //         await this.iframe.getByRole('radio', { name: 'Bank account' }).click(),
-    //         await this.iframe.getByRole('button', { name: 'Complete purchase' }).click()
-                
-    //         await Promise.race([
-    //             this.page.waitForURL(/mock/),
-    //             this.page.waitForResponse(response => response.url().includes('pco_wc_update_checkout') && response.status() === 200)
-    //             //new Promise(f => setTimeout(f, 10000))
-    //         ])
-            
-    //     }
-    // }
-
     async finishOrder() {
         await this.iframe.locator('#SubmitAddress').click();
 
-        await Promise.race([
-            new Promise(f => setTimeout(f, 10000)),
-            this.page.waitForResponse(response => response.url().includes('pco_wc_update_checkout') && response.status() === 200)
-        ]);
-        await Promise.race([
-            new Promise(f => setTimeout(f, 10000)),
-            this.page.waitForResponse(response => response.url().includes('pco_wc_update_checkout') && response.status() === 200)
-        ]);
+        // Paysoncheckout will trigger a update twice after the addess is submitted. Once for the address being changed event, and one when the address fields are populated in WooCommerce
+        await this.page.waitForResponse(response => response.url().includes('pco_wc_update_checkout') && response.status() === 200);
+        await this.page.waitForResponse(response => response.url().includes('pco_wc_update_checkout') && response.status() === 200);
 
-        await this.iframe.getByRole('radio', { name: 'Bank account' }).click(),
-        await this.iframe.getByRole('button', { name: 'Complete purchase' }).click()
+        // Select the bank account payment method and complete the purchase.
+        await this.iframe.getByRole('radio', { name: 'Bank account' }).click();
+        await this.iframe.getByRole('button', { name: 'Complete purchase' }).click();
 
+        // Simulate the accept button being clicked landing on the mock page.
         await this.page.getByRole('button', { name: 'Simulate Accept' }).click();
     }
 
     async handleIFrame() {
-        // await Promise.race([
-            
-        //     new Promise(f => setTimeout(f, 10000))
-        // ]);
-
         await this.fillPaysonPaymentDetails();
-        
+
         await this.finishOrder();
     }
 }
