fdc5f6455ce64c511b114f03f3f0252b0d2a1f23
anyakro <anya@krokedil.se>
2024-10-15 13:47:53 +0200

    Add checks to avoid fatal error in logger

--- classes/testdiff.php
+++ classes/testdiff.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * Logger class file.
+ * Logging class file.
  *
- * @package PaysonCheckout/Classes
+ * @package Qliro_One_For_WooCommerce/Classes
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+	exit;
 }
 
 /**
- * Logger class.
+ * Qliro_One_Logger class.
  */
-class PaysonCheckout_For_WooCommerce_Logger {
+class Qliro_One_Logger {
 	/**
 	 * Log message string
 	 *
 	 * @var $log
 	 */
-	public static $log;
+	private static $log;
 
 	/**
 	 * Logs an event.
@@ -26,13 +26,14 @@ class PaysonCheckout_For_WooCommerce_Logger {
 	 * @param string $data The data string.
 	 */
 	public static function log( $data ) {
-		$payson_settings = get_option( 'woocommerce_paysoncheckout_settings' );
-		if ( 'yes' === $payson_settings['debug'] ) {
+		$settings = get_option( 'woocommerce_qliro_one_settings' );
+
+		if ( 'yes' === $settings['logging'] ) {
 			$message = self::format_data( $data );
 			if ( empty( self::$log ) ) {
 				self::$log = new WC_Logger();
 			}
-			self::$log->add( 'paysoncheckout', wp_json_encode( $message ) );
+			self::$log->add( 'qliro-one-checkout-for-woocommerce', wp_json_encode( $message ) );
 		}
 
 		if ( isset( $data['response']['code'] ) && ( $data['response']['code'] < 200 || $data['response']['code'] > 299 ) ) {
@@ -48,54 +49,49 @@ class PaysonCheckout_For_WooCommerce_Logger {
 	 */
 	public static function format_data( $data ) {
 		if ( isset( $data['request']['body'] ) ) {
-			$request_body            = json_decode( $data['request']['body'], true );
-			$data['request']['body'] = $request_body;
+			$data['request']['body'] = json_decode( $data['request']['body'], true );
 		}
-
 		return $data;
 	}
 
 	/**
 	 * Formats the log data to be logged.
 	 *
-	 * @param string $payment_id The Payson Payment ID.
-	 * @param string $method The method.
-	 * @param string $title The title for the log.
-	 * @param array  $request_args The request args.
-	 * @param array  $response The response.
-	 * @param string $code The status code.
+	 * @param string          $qliro_order_id The Qliro One order id.
+	 * @param string          $method The method.
+	 * @param string          $title The title for the log.
+	 * @param array           $request_args The request args.
+	 * @param object|WP_Error $response The response.
+	 * @param string          $code The status code.
+	 * @param string          $request_url The request url.
 	 * @return array
 	 */
-	public static function format_log( $payment_id, $method, $title, $request_args, $response, $code ) {
-		// Unset the snipp et to prevent issues in the response.
-		if ( isset( $response['snippet'] ) ) {
-			unset( $response['snippet'] );
+	public static function format_log( $qliro_order_id, $method, $title, $request_args, $response, $code, $request_url = null ) {
+		// Unset the snippet to prevent issues in the response.
+		if ( ! is_wp_error( $response ) ) {
+			$response = self::remove_html_snippet( $response );
 		}
-
 		// Unset the snippet to prevent issues in the request body.
 		if ( isset( $request_args['body'] ) ) {
 			$request_body = json_decode( $request_args['body'], true );
-			if ( isset( $request_body['snippet'] ) && $request_body['snippet'] ) {
-				unset( $request_body['snippet'] );
+			if ( isset( $request_body['OrderHtmlSnippet'] ) ) {
+				unset( $request_body['OrderHtmlSnippet'] );
 				$request_args['body'] = wp_json_encode( $request_body );
 			}
 		}
 		return array(
-			'id'             => $payment_id,
+			'id'             => $qliro_order_id,
 			'type'           => $method,
 			'title'          => $title,
 			'request'        => $request_args,
+			'request_url'    => $request_url,
 			'response'       => array(
 				'body' => $response,
 				'code' => $code,
 			),
-			'timestamp'      => date( 'Y-m-d H:i:s' ),
-			'plugin_version' => PAYSONCHECKOUT_VERSION,
-			'php_version'    => phpversion(),
-			'wc_version'     => WC()->version,
-			'wp_version'     => get_bloginfo( 'version' ),
-			'user_agent'     => wc_get_user_agent(),
-			'stacktrace'     => self::get_stack(),
+			'timestamp'      => date( 'Y-m-d H:i:s' ), // phpcs:ignore WordPress.DateTime.RestrictedFunctions -- Date is not used for display.
+			'stack'          => self::get_stack(),
+			'plugin_version' => QLIRO_WC_VERSION,
 		);
 	}
 
@@ -129,7 +125,7 @@ class PaysonCheckout_For_WooCommerce_Logger {
 	 * @param array $data The data to be logged.
 	 */
 	public static function log_to_db( $data ) {
-		$logs = get_option( 'krokedil_debuglog_payson', array() );
+		$logs = get_option( 'krokedil_debuglog_qliro_one', array() );
 
 		if ( ! empty( $logs ) ) {
 			$logs = json_decode( $logs );
@@ -138,6 +134,24 @@ class PaysonCheckout_For_WooCommerce_Logger {
 		$logs   = array_slice( $logs, -14 );
 		$logs[] = $data;
 		$logs   = wp_json_encode( $logs );
-		update_option( 'krokedil_debuglog_payson', $logs, false );
+		update_option( 'krokedil_debuglog_qliro_one', $logs );
+	}
+
+	/**
+	 * Removes the HTML snippet from the response body if its set.
+	 *
+	 * @param object $response
+	 * @return object
+	 */
+	public static function remove_html_snippet( $response ) {
+		$body = json_decode( wp_remote_retrieve_body( $response ), true );
+
+		if ( isset( $body['OrderHtmlSnippet'] ) ) {
+			unset( $body['OrderHtmlSnippet'] );
+		}
+
+		$response['body'] = $body;
+
+		return $response;
 	}
 }
