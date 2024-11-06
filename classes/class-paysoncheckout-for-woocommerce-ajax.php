759ad34e73b0bb1c9e1ee0fb819af9934ee0945f
anyakro <anya@krokedil.se>
2024-11-06 12:02:59 +0100

    Check for renewal in subscription

--- classes/class-paysoncheckout-for-woocommerce-ajax.php
+++ classes/class-paysoncheckout-for-woocommerce-ajax.php
@@ -110,16 +110,7 @@ class PaysonCheckout_For_WooCommerce_AJAX extends WC_AJAX {
 			);
 		}
 
-		$subscription = false;
-		if ( class_exists( 'WC_Subscriptions_Cart' ) ) {
-			foreach ( WC()->cart->get_cart() as $cart_item ) {
-				$product_id = $cart_item['product_id'];
-				if ( WC_Subscriptions_Product::is_subscription( $product_id ) ) {
-					$subscription = true;
-					break;
-				}
-			}
-		}
+		$subscription = ( class_exists( 'WC_Subscriptions_Cart' ) && ( WC_Subscriptions_Cart::cart_contains_subscription() || wcs_cart_contains_renewal() ) ) ? true : false;
 
 		if ( ! $subscription && ! WC()->cart->needs_payment() ) {
 			wp_send_json_success(
@@ -205,7 +196,7 @@ class PaysonCheckout_For_WooCommerce_AJAX extends WC_AJAX {
 		}
 
 		// Get the payson order.
-		$subscription = ( class_exists( 'WC_Subscriptions_Cart' ) && WC_Subscriptions_Cart::cart_contains_subscription() ) ? true : false;
+		$subscription = ( class_exists( 'WC_Subscriptions_Cart' ) && ( WC_Subscriptions_Cart::cart_contains_subscription() || wcs_cart_contains_renewal() ) ) ? true : false;
 
 		$payson_order = ( $subscription ) ? PCO_WC()->get_recurring_order->request( WC()->session->get( 'payson_payment_id' ) ) : PCO_WC()->get_order->request( WC()->session->get( 'payson_payment_id' ) );
 		if ( is_wp_error( $payson_order ) ) {
